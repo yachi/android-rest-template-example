8f907c029ad90415320fde95d89ce1eaec2b40ef
yachi <yaachi@gmail.com>
2012-09-30 23:12:04 +0800

    omg

--- src/org/newsweb/newsweb.java
+++ src/org/newsweb/newsweb.java
@@ -2,6 +2,10 @@ package org.newsweb;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.converter.StringHttpMessageConverter;
 
 public class newsweb extends Activity
 {
@@ -10,6 +14,12 @@ public class newsweb extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        String url = "http://hknewsweb.herokuapp.com/api/apple/sections";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        String result = restTemplate.getForObject(url, String.class);
         setContentView(R.layout.main);
+        TextView tv = (TextView) findViewById(R.id.text);
+        tv.setText(result);
     }
 }
