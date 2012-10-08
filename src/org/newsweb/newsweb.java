b0cec25fb0c56dcf26b08ccbf44c5c89e4b6e5f6
yachi <yaachi@gmail.com>
2012-10-09 00:38:08 +0800

    should use after views

--- src/org/newsweb/newsweb.java
+++ src/org/newsweb/newsweb.java
@@ -1,30 +1,28 @@
 package org.newsweb;
 
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ViewById;
 
-@EActivity
+@EActivity(R.layout.main)
 public class newsweb extends Activity
 {
     @ViewById(R.id.text)
     TextView tv;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+    @AfterViews
+    void setText() {
         String url = "http://hknewsweb.herokuapp.com/api/apple/sections";
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
         SectionModel []  sections = restTemplate.getForObject(url, SectionModel[].class);
-        setContentView(R.layout.main);
         tv.setText(sections[0].getLink());
     }
 }
